f217c583a0056b08b1f6d996a40fe5b1c41968d6
Mark Killian <Killianmo@gmail.com>
2016-02-23 19:40:27 -0600

    Plagerized

--- exercise-5/exercise-5.js
+++ exercise-5/exercise-5.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-  // Write your code here
+  $('form').on('submit', function(event){
+    event.preventDefault();
+    var zipCode = $('form input').val();
+    if(zipCode.length === 5 && Number(zipCode)){
+      // We know the zip code is valid
+      // Make our GET request
+      $.get('http://maps.googleapis.com/maps/api/geocode/json', {address: zipCode}, function(responseData){
+        var lat = responseData.results[0].geometry.location.lat;
+        var lng = responseData.results[0].geometry.location.lng;
+        console.log(lat, lng)
+      });
+    } else {
+      // We know the zip code isn't valid
+      alert('You didn\'t enter a zip code!');
+    }
+  });
 });
